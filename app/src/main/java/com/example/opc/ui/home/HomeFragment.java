ab4562baee61273521e78db19a580f831c19dcd9
Prajwal Rothe <201751035@iiitvadodara.ac.in>
2019-11-17 22:35:10 +0530

    notification part done!

--- app/src/main/java/com/example/opc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/opc/ui/home/HomeFragment.java
@@ -21,32 +21,87 @@ import com.example.opc.Alumni_main;
 import com.example.opc.MainActivity;
 import com.example.opc.R;
 import com.example.opc.TPO_add_company;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Iterator;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    Activity context;
+//    Activity context;
+private TextView viewNotif;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference rootref = db.getReference();
+    private  DatabaseReference notifRef = rootref.child("notification");
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser currentUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        context=getActivity();
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        viewNotif = v.findViewById(R.id.dispNotif);
+        currentUser = mAuth.getCurrentUser();
+        String myEmail = currentUser.getEmail();
+        int index = myEmail.indexOf('@');
+        String node = myEmail.substring(0,index);
+        DatabaseReference mesRef = notifRef.child(node);
+
+        mesRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                appendNotifications(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                appendNotifications(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
-        return root;
+
+        return v;
+    }
+
+    public void appendNotifications(DataSnapshot dataSnapshot)
+    {
+        String notif,date,time;
+        Iterator i = dataSnapshot.getChildren().iterator();
+        while(i.hasNext()){
+            date = (String)((DataSnapshot) i.next()).getValue();
+            notif = (String)((DataSnapshot) i.next()).getValue();
+            time = (String)((DataSnapshot) i.next()).getValue();
+
+            viewNotif.append(notif+'\n'+date+'\n'+time+'\n'+'\n');
+        }
+
     }
     public void onStart(){
         super.onStart();
-        Button bt=(Button)context.findViewById(R.id.bt);
+        /*Button bt=(Button)context.findViewById(R.id.bt);
         Button bt2=(Button)context.findViewById(R.id.bt2);
-        /*bt.setOnClickListener(new View.OnClickListener(){
+        bt.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
                 Intent intent=new Intent(context, Alumni_main.class);
